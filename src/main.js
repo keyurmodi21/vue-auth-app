74687533091dfee1a18b6ff15088dabdfb378569
Keyur Modi <keyurmodi@keyur-ma.local>
2020-04-05 10:57:07 +0800

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import { router } from './router';
+import store from './store';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import VeeValidate from 'vee-validate';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+  faHome,
+  faUser,
+  faUserPlus,
+  faSignInAlt,
+  faSignOutAlt, faKey
+} from '@fortawesome/free-solid-svg-icons';
 
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt, faKey);
 
-Vue.use(BootstrapVue)
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VeeValidate);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
---
+++ src/views/Login.vue
@@ -0,0 +1,231 @@
+<template>
+  <!-- <div class="col-md-12">
+    <div class="card card-container">
+      <img
+        id="profile-img"
+        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+        class="profile-img-card"
+      />
+      <form name="form" @submit.prevent="handleLogin">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input
+            v-model="user.username"
+            v-validate="'required'"
+            type="text"
+            class="form-control"
+            name="username"
+          />
+          <div
+            v-if="errors.has('username')"
+            class="alert alert-danger"
+            role="alert"
+          >Username is required!</div>
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input
+            v-model="user.password"
+            v-validate="'required'"
+            type="password"
+            class="form-control"
+            name="password"
+          />
+          <div
+            v-if="errors.has('password')"
+            class="alert alert-danger"
+            role="alert"
+          >Password is required!</div>
+        </div>
+        <div class="form-group">
+          <button class="btn btn-primary btn-block" :disabled="loading">
+            <span v-show="loading" class="spinner-border spinner-border-sm"></span>
+            <span>Login</span>
+          </button>
+        </div>
+        <div class="form-group">
+          <div v-if="message" class="alert alert-danger" role="alert">{{message}}</div>
+        </div>
+      </form>
+    </div>
+  </div>-->
+
+  <div class="col-md-12">
+    <div class="card-container mx-auto">
+      <div class="text-center">
+        <img src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" class="profile-image" />
+      </div>
+      <form class="mx-auto form-width" @submit.prevent="handleLogin">
+        <label>Username</label>
+
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">
+              <font-awesome-icon icon="user" />
+            </span>
+          </div>
+          <input
+            type="text"
+            v-model="user.username"
+            v-validate="'required'"
+            class="form-control"
+            placeholder="Username"
+            name="username"
+          />
+          <div
+            v-if="errors.has('username')"
+            class="alert alert-danger"
+            role="alert"
+          >Username is required!</div>
+        </div>
+
+        <label>Password</label>
+
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <font-awesome-icon icon="key" />
+            </span>
+          </div>
+          <input
+            type="password"
+            v-model="user.password"
+            v-validate="'required'"
+            class="form-control"
+            placeholder="Password"
+            name="password"
+          />
+          <div
+            v-if="errors.has('password')"
+            class="alert alert-danger"
+            role="alert"
+          >Password is required!</div>
+        </div>
+
+        <div class="text-center mTop30">
+          <input class="btn btn-info btn-block" type="submit" value="Login" />
+        </div>
+        <div class="form-group">
+          <div v-if="message" class="alert alert-danger" role="alert">{{message}}</div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import User from "../models/user";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      user: new User("", ""),
+      loading: false,
+      message: ""
+    };
+  },
+  computed: {
+    loggedIn() {
+      return this.$store.state.auth.status.loggedIn;
+    }
+  },
+  created() {
+    if (this.loggedIn) {
+      this.$router.push("/profile");
+    }
+  },
+  methods: {
+    handleLogin() {
+      this.loading = true;
+
+      this.$validator.validateAll().then(isValid => {
+        if (!isValid) {
+          this.loading = false;
+          return;
+        }
+
+        if (this.user.username && this.user.password) {
+          this.$store.dispatch("auth/login", this.user).then(
+            () => {
+              this.$router.push("/profile");
+            },
+            error => {
+              this.loading = false;
+              this.message =
+                (error.response && error.response.data) ||
+                error.message ||
+                error.toString();
+            }
+          );
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+label {
+  display: block;
+  margin-top: 10px;
+}
+
+.card-container.card {
+  max-width: 350px !important;
+  padding: 40px 40px;
+}
+
+.card {
+  background-color: #f7f7f7;
+  padding: 20px 25px 30px;
+  margin: 0 auto 25px;
+  margin-top: 50px;
+  -moz-border-radius: 2px;
+  -webkit-border-radius: 2px;
+  border-radius: 2px;
+  -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+}
+
+.profile-img-card {
+  width: 96px;
+  height: 96px;
+  margin: 0 auto 10px;
+  display: block;
+  -moz-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  border-radius: 50%;
+}
+
+.mTop30 {
+  margin-top: 30px;
+}
+
+.form-width {
+  width: 200px;
+}
+
+.card-container {
+  border: 2px;
+  padding: 30px 10px;
+  width: 400px;
+  margin-top: 50px;
+  background-color: #f7f7f7;
+  -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  -moz-border-radius: 2px;
+  -webkit-border-radius: 2px;
+  border-radius: 2px;
+}
+
+.profile-image {
+  width: 96px;
+  height: 96px;
+  border-radius: 50%;
+  -moz-border-radius: 50%;
+  -webkit-border-radius: 50%;
+}
+</style>
